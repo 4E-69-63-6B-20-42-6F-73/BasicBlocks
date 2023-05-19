cc105b263af79d3bd426e49da5610fc51a642c0a
Nick <20791144+4E-69-63-6B-20-42-6F-73@users.noreply.github.com>
2023-05-19 20:13:59 +0200

    added sign in example

--- dist/app.js
+++ dist/app.js
@@ -36,6 +36,32 @@ class BaseComponent extends HTMLElement {
         // Implemented in child
     }
   }
+class CustomCheckbox extends BaseComponent {
+    static get observedAttributes() {
+      return ['checked', 'disabled'];
+    }
+  
+    setup() {
+      this.classList.add('check');
+    }
+  
+    render() {
+      const isChecked = this.hasAttribute('checked');
+      const name = this.getAttribute('name') || '';
+      const isDisabled = this.hasAttribute('disabled');
+  
+      this.innerHTML = `
+        <label class="check-label ${isDisabled ? 'disabled' : ''}">
+          <input name="${name}" class="check-input" type="checkbox" value=""
+            ${isChecked ? 'checked' : ''} ${isDisabled ? 'disabled' : ''}> 
+          ${this.textContent}
+        </label>
+      `;
+    }
+  }
+  
+  customElements.define('check-box', CustomCheckbox);
+  
 class CodeSnippet extends BaseComponent {
     constructor() {
         super();
